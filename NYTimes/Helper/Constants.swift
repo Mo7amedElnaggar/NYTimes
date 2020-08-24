1638bcfbb4f0e709094ade9fdd6c315c5f4ae980
Mo7amedElnaggar <iNagggar@gmail.com>
2020-08-24 20:14:26 +0200

    Constants

---
+++ NYTimes/Helper/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NYTimes
+//
+//  Created by Mohamed El-Naggar on 8/24/20.
+//  Copyright © 2020 Mohamed El-Naggar. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let APIKey = "elDS1dv8Eoxip8XmHSbX1jrw0DiGLPSn"
+    static let APIUrl = "https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json"
+}
