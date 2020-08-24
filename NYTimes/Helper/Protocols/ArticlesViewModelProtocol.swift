1544807422590b275e84db6d61563010724f71d2
Mo7amedElnaggar <iNagggar@gmail.com>
2020-08-24 20:15:29 +0200

    ArticleViewModel CallBack

---
+++ NYTimes/Helper/Protocols/ArticlesViewModelProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ArticlesViewModelProtocol.swift
+//  NYTimes
+//
+//  Created by Mohamed El-Naggar on 8/24/20.
+//  Copyright © 2020 Mohamed El-Naggar. All rights reserved.
+//
+
+import Foundation
+
+protocol ArticlesViewModelDelegate {
+    func didRecieveArticles(articles: MostPopularArticles)
+    func didRecieveError(errMsg: String)
+}
